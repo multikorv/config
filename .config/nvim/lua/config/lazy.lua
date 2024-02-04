dc66f4bbe5b94ff9a61ceaf3186d611663d238c7
Marcus Antonsson <94271+multikorv@users.noreply.github.com>
2024-02-04 11:34:08 +0100

    Add telescope file browser

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,45 +1,53 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-    "williamboman/mason.nvim",
+require('lazy').setup({
+    'williamboman/mason.nvim',
     {
-        "williamboman/mason-lspconfig.nvim", 
+        'williamboman/mason-lspconfig.nvim', 
         lazy = false 
     },
-    "lewis6991/gitsigns.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/vim-vsnip",
-    "neovim/nvim-lspconfig",
-    "nvim-lua/plenary.nvim",
-    "famiu/feline.nvim",
-    "folke/tokyonight.nvim",
+    'lewis6991/gitsigns.nvim',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+    'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
+    'famiu/feline.nvim',
+    'folke/tokyonight.nvim',
     { 
-        "rose-pine/neovim", 
-        name = "rose-pine" 
+        'rose-pine/neovim', 
+        name = 'rose-pine' 
     },
     {
-        "nvim-telescope/telescope.nvim", 
-        tag = "0.1.5", 
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.5', 
         dependencies = 
         { 
-            "nvim-lua/plenary.nvim" 
+            'nvim-lua/plenary.nvim' 
         }
     },
     {
-        "nvim-treesitter/nvim-treesitter", 
-        build = ":TSUpdate"
+        'nvim-telescope/telescope-file-browser.nvim',
+        dependencies = 
+        { 
+            'nvim-telescope/telescope.nvim', 
+            'nvim-lua/plenary.nvim' 
+        }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter', 
+        build = ':TSUpdate'
     }
 })
--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,7 +1,32 @@
 local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>tt', builtin.git_files, {})
+
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
+--[[vim.keymap.set('n', '<leader>tc', function() 
+    builtin.find_files({ search_dirs = {'~/.config/nvim'} })
+end, {})]]--
+
+
+
 vim.keymap.set('n', '<leader>tg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ts', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
+
+
+-- open file_browser with the path of the current buffer
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>to',
+  ':Telescope file_browser path=%:p:h select_buffer=true<CR>',
+  { noremap = true }
+)
+
+-- open file_browser over nvim config files
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>tc',
+  ':Telescope file_browser path=~/.config/nvim select_buffer=true<CR>',
+  { noremap = true }
+)
