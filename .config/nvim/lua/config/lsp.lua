948316971f71b1160b493ae3ea362234130123eb
Marcus Antonsson <94271+multikorv@users.noreply.github.com>
2024-06-16 15:32:24 +0200

    Add autocmd to launch pico8-lsp

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -85,5 +85,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
             opts
         )
     end,
-}
-)
+})
+
+-- Specific for getting pico8-ls client to start
+vim.api.nvim_create_autocmd({'BufNew', 'BufEnter'}, {
+    pattern = { '*.p8' },
+    callback = function(args)
+        vim.lsp.start({
+            name = 'pico8-ls',
+            cmd = { 'pico8-ls', '--stdio' },
+            root_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf)),
+            -- Setup your keybinds in the on_attach function
+            --on_attach = on_attach,
+        })
+    end
+})
+
+
