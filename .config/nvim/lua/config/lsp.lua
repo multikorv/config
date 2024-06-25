9e0cf58bc1807d32dcf106c0ec1a133bc23d89d1
Marcus Antonsson <94271+multikorv@users.noreply.github.com>
2024-06-25 09:35:13 +0200

    Use <leader> instead of <space> in lsp.lua

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -77,7 +77,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
 
         vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
 
         vim.keymap.set('n', '<leader>f', function()
                 vim.lsp.buf.format { async = true }
