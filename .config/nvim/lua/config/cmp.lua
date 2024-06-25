901fca3b2ac9bd5c357f1fc9f6ae6d0b7f8d7664
Marcus Antonsson <94271+multikorv@users.noreply.github.com>
2024-06-25 09:35:51 +0200

    Add buffer as source in cmp.lua

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -29,6 +29,7 @@ cmp.setup({
         { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
+        { name = 'buffer' },
     }, {
         { name = 'buffer' },
     })
